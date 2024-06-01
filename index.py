175b6fee80a651e7123e7b6aa652d8f1b4108c05
Yoan Bassinet <85518363+yoanbst@users.noreply.github.com>
2024-06-01 23:27:10 +0200

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.title("CSV Data Analysis")
+
 # File uploader
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
